876f928260fcb5c0432a0712e85e8962ff9c5410
snychka <stefannychka@gmail.com>
2019-12-27 14:25:43 -0700

    copy over private repo.'s files

---
+++ ConferenceTracker/Entities/Speaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceTracker.Entities
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public bool IsStaff { get; set; }
+    }
+}
