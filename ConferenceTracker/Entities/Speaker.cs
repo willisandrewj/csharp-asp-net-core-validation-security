e6ca4e56630ad9c142304ff1db71573586dee529
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-03 13:13:34 -0500

    Added the Required Attribute to Speaker

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,11 +6,15 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
         [Display(Name = "First name")]
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Required]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
