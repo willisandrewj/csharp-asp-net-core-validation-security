62a7d9588f8355d82c5817c0357e76c7cb2d4767
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-03 13:19:07 -0500

    Added StringLengthe attribute to Speaker properties

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -10,14 +10,17 @@ namespace ConferenceTracker.Entities
         public int Id { get; set; }
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
         [Required]
         [DataType(DataType.MultilineText)]
+        [StringLength(500, MinimumLength = 10)]
         public string Description { get; set; }
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
