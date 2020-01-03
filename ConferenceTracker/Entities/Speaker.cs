cfe5853d7f955cb8f77f62f40d0ec28ef3eefabd
Eric J Fisher <eric-fisher@pluralsight.com>
2020-01-03 13:17:09 -0500

    Added DataType attribute to Speaker properties

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -8,16 +8,21 @@ namespace ConferenceTracker.Entities
     {
         [Required]
         public int Id { get; set; }
-        [Display(Name = "First name")]
         [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
         [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
         [Required]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
